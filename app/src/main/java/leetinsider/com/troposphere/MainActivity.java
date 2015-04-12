f9a71ce1840c88e99cfb7bd1d400d9861f790f73
wabassett <wabassett@gmail.com>
2015-04-12 10:36:50 -0700

    Implements location services and geocoding

--- app/src/main/java/leetinsider/com/troposphere/MainActivity.java
+++ app/src/main/java/leetinsider/com/troposphere/MainActivity.java
@@ -1,20 +1,28 @@
 package leetinsider.com.troposphere;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Handler;
+import android.os.ResultReceiver;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.ConnectionResult;
+
+import android.content.Intent;
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -26,7 +34,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.net.NetworkInterface;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -34,7 +41,8 @@ import butterknife.InjectView;
 /**
  * Created by FinalTrigger on 1/24/15.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements
+        ConnectionCallbacks, OnConnectionFailedListener  {
     //Create TAG variable for logging on MainActivity
     public static final String TAG = MainActivity.class.getSimpleName();
 
@@ -48,6 +56,16 @@ public class MainActivity extends ActionBarActivity {
     @InjectView(R.id.iconImageView) ImageView mIconImageView;
     @InjectView(R.id.refreshImageView) ImageView mRefreshImageView;
     @InjectView(R.id.progressBar) ProgressBar mProgressBar;
+    @InjectView(R.id.locationLabel) TextView mLocationLabel;
+
+    private Location mLastLocation;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private AddressResultReceiver mResultReceiver;
+
+    private String mAddressFromGeoCoder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,21 +76,23 @@ public class MainActivity extends ActionBarActivity {
 
         //Hide progress spinner on load
         mProgressBar.setVisibility(View.INVISIBLE);
+        buildGoogleApiClient();
 
-        //make lat and long global for most functions
-        //Used for parse user location for later feature rather than hardcode location
-        final double latitude = 40.7242;
-        final double longitude = -74.1726;
+        mGoogleApiClient.connect();
+
+        mResultReceiver = new AddressResultReceiver(new Handler());
 
         //Set click listener for refresh - call getForecast function
         mRefreshImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getForecast(latitude, longitude);
+                if (mLastLocation != null) {
+                    getForecast(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+                    getLocationName();
+                }
             }
         });
         //Start app by getting forecast.io data
-        getForecast(latitude, longitude);
         //Log letting dev know activity is running
         Log.d(TAG, "Main UI code is running!");
     }
@@ -155,6 +175,59 @@ public class MainActivity extends ActionBarActivity {
                     Toast.LENGTH_LONG).show();
         }
     }
+    protected synchronized void buildGoogleApiClient() {
+        Log.d(TAG,"Building Google API");
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        Log.d(TAG,"Completed Build Google API");
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.d(TAG,"Connected to Google");
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation == null) {
+            Log.e(TAG,"Null Last location");
+        }
+        else{
+            Log.d(TAG,String.format("Retrieved location latitude: [%f], Longitude: [%f]",mLastLocation.getLatitude(), mLastLocation.getLongitude()));
+            getForecast(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            getLocationName();
+        }
+    }
+    @Override
+    public void onConnectionSuspended(int cause){
+        Log.e(TAG,"Connection to Google suspended.");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result){
+         if (result.hasResolution()) {
+             Log.e(TAG,"Connection to Google failed.  Attempting reconnect.");
+             mGoogleApiClient.connect();
+        }
+         else {
+             Log.e(TAG,"Connection to Google failed.  No reconnect attempt.");
+         }
+    }
+    protected void getLocationName() {
+        // Create an intent for passing to the intent service responsible for fetching the address.
+        Intent intent = new Intent(this, FetchAddressIntentService.class);
+
+        // Pass the result receiver as an extra to the service.
+        intent.putExtra(Constants.RECEIVER, mResultReceiver);
+
+        // Pass the location data as an extra to the service.
+        intent.putExtra(Constants.LOCATION_DATA_EXTRA, mLastLocation);
+
+        // Start the service. If the service isn't already running, it is instantiated and started
+        // (creating a process for it if needed); if it is running then it remains running. The
+        // service kills itself automatically once all intents are processed.
+        startService(intent);
+    }
 
     private void toggleRefresh() {
         /*
@@ -173,15 +246,21 @@ public class MainActivity extends ActionBarActivity {
 
     //Update the activity main views to the variables from the data in CurrentWeather
     private void updateDisplay() {
-        mTemperatureLabel.setText(mCurrentWeather.getTemperature() + "");
-        mTimeLabel.setText("At " + mCurrentWeather.getFormattedTime() + " it is currently");
-        mHumidityValue.setText(mCurrentWeather.getHumidity() + "");
-        mPrecipValue.setText(mCurrentWeather.getPrecipChance() + "%");
-        mSummaryLabel.setText(mCurrentWeather.getSummary());
+        if (mCurrentWeather != null){
+            mTemperatureLabel.setText(mCurrentWeather.getTemperature() + "");
+            mTimeLabel.setText("At " + mCurrentWeather.getFormattedTime() + " it is currently");
+            mHumidityValue.setText(mCurrentWeather.getHumidity() + "");
+            mPrecipValue.setText(mCurrentWeather.getPrecipChance() + "%");
+            mSummaryLabel.setText(mCurrentWeather.getSummary());
 
-        //Update the weather icon
-        Drawable drawable = getResources().getDrawable(mCurrentWeather.getIconId());
-        mIconImageView.setImageDrawable(drawable);
+            //Update the weather icon
+            Drawable drawable = getResources().getDrawable(mCurrentWeather.getIconId());
+            mIconImageView.setImageDrawable(drawable);
+        }
+
+        if (mAddressFromGeoCoder != null){
+            mLocationLabel.setText(mAddressFromGeoCoder);
+        }
     }
 
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
@@ -218,17 +297,39 @@ public class MainActivity extends ActionBarActivity {
         ConnectivityManager manager = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = manager.getActiveNetworkInfo();
-        boolean isAvailable = false;
-        if (networkInfo != null && networkInfo.isConnected()) {
-            isAvailable = true;
-        }
-
-        return isAvailable;
+        return (networkInfo != null && networkInfo.isConnected());
     }
 
     private void alertUserAboutError() {
         AlertDialogFragment dialog = new AlertDialogFragment();
         dialog.show(getFragmentManager(), "error_dialog");
     }
+
+    class AddressResultReceiver extends ResultReceiver {
+        public AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+        /*
+         *  Receives data sent from FetchAddressIntentService and updates the UI in MainActivity.
+         */
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+
+            // Display the address string or an error message sent from the intent service.
+
+            // Show a toast message if an address was found.
+            if (resultCode == Constants.SUCCESS_RESULT) {
+                mAddressFromGeoCoder = resultData.getString(Constants.RESULT_DATA_KEY);
+                Log.d(TAG,String.format("Address [%s] was found", mAddressFromGeoCoder));
+            }
+            else
+            {
+                Log.e(TAG,"No Address returned geocoder!");
+                mAddressFromGeoCoder = "No Address Found";
+            }
+            updateDisplay();
+        }
+    }
+
 }
 
